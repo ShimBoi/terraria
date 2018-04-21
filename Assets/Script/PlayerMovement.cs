dee171a38ccb254cf00d0bfacb953d6c5d1c230b
unknown <tgsoon2002@gmail.com>
2018-04-21 10:27:52 -0700

    first setup

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+Rigidbody2D rigid ;
+public float Timer;
+	// Use this for initialization
+	void Start () {
+		rigid = gameObject.GetComponent<Rigidbody2D>();
+        Timer = 0;
+	}
+	bool onGround = true;
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;
+		if (Input.GetKeyDown("w") && onGround == true){
+
+			rigid. AddRelativeForce(new Vector2(0,10), ForceMode2D.Impulse);
+			onGround = false;
+		} 
+		if (Input.GetKey("d")){
+            rigid.AddRelativeForce(new Vector2(10, 0), ForceMode2D.Force);
+		}
+        if (Input.GetKey("a"))
+        {
+            rigid.AddRelativeForce(new Vector2(-10, 0), ForceMode2D.Force);
+        }
+        if (Input.GetKey("s"))
+        {
+			Timer = 0;
+			GetComponent<Collider2D>().enabled = false;
+        }
+		RaycastHit hitInfo;
+        //hitInfo.transform.gameObject.GetComponent<tree>().IAMMElted();
+		if (Timer > 0.5){
+            GetComponent<Collider2D>().enabled = true;
+		}
+        // GetComponent<Collider2D>().enabled=! Input.GetKey("s"); // true when press down
+
+        //  want to turn off when down
+
+	
+		
+	} 
+
+	void OnCollision2D(Collider2D col){
+		if (col.gameObject.CompareTag ("terrain")) {
+				onGround = true;
+			}
+
+	}
+}
